51d238683e81f1345ab238424eab922f6429ae96
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2023-03-07 17:48:28 +0700

    report pengaduan

--- application/views/user/pengaduan.php
+++ application/views/user/pengaduan.php
@@ -220,7 +220,6 @@
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-					<button type="submit" class="btn btn-primary">Save changes</button>
 				</div>
 			</div>
 			</form>
